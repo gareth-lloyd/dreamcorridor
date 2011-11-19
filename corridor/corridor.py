ad1cbb477ffa29c65df672af4acb6cfafd06cff3
Ben Kirman <ben@kirman.org>
2011-11-19 20:16:48 +0000

    added colour sensor

--- corridor/corridor.py
+++ corridor/corridor.py
@@ -6,19 +6,18 @@ from opencv import highgui
 import opencv
 import Image
 
+colours = ["black","blue","green","yellow","red","white"]
 b = nxt.locator.find_one_brick()
-camera = highgui.cvCreateCameraCapture(1)
-threshold = 5
-baseline = Ultrasonic(b, PORT_4).get_sample()
+camera = highgui.cvCreateCameraCapture(1)#initial camera setup, presuming it is the 2nd (i.e. pos 1) camera
+threshold = 5 #amount of difference to trigger event
+sleeptime = 5 #sleep time to prevent multiple images
+baseline = Ultrasonic(b, PORT_4).get_sample() #get baseline distance measurement from sensor.
 print "baseline sampled at "+str(baseline)+"cm"
 def get_image():
     print "taking photo"
-    #camera = highgui.cvCreateCameraCapture(1)
     im = highgui.cvQueryFrame(camera)
-    #im = opencv.cvGetMat(im)
+    
     #convert Ipl image to PIL image
-    #highgui.cvReleaseCapture(camera)
-    #camera = highgui.cvCreateCameraCapture(1)
     return opencv.adaptors.Ipl2PIL(im)
 
 
@@ -27,9 +26,10 @@ while(True):
     if (sample+threshold)<baseline:
         print "person detected at " + str(sample)+"cm"
         im = get_image()
-	highgui.cvReleaseCapture(camera)
-	camera = highgui.cvCreateCameraCapture(1)
-	print "saving photo"
+        print colours[Color20(b,PORT_3).get_color()]
+    	print "saving photo"
         im.save('test'+time.strftime('%H%M%S')+'.jpg','JPEG')
-    	time.sleep(10)
+        highgui.cvReleaseCapture(camera)#reset camera
+        camera = highgui.cvCreateCameraCapture(1)
+    	time.sleep(sleeptime)
 
