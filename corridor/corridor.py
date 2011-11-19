31c36d8f493d0d8edde90f9850cc848d2d8572db
Ben Kirman <ben@kirman.org>
2011-11-19 19:15:41 +0000

    Initial trigger code

---
+++ corridor/corridor.py
@@ -0,0 +1,31 @@
+import nxt.locator
+import time
+from nxt.sensor import *
+
+from opencv import highgui
+import opencv
+import Image
+
+b = nxt.locator.find_one_brick()
+camera = highgui.cvCreateCameraCapture(1)
+threshold = 5
+baseline = Ultrasonic(b, PORT_4).get_sample()
+print "baseline sampled at "+str(baseline)+"cm"
+def get_image():
+    print "taking photo"
+    #camera = highgui.cvCreateCameraCapture(1)
+    im = highgui.cvQueryFrame(camera)
+    #im = opencv.cvGetMat(im)
+    #convert Ipl image to PIL image
+    return opencv.adaptors.Ipl2PIL(im)
+
+
+while(True):
+    sample = Ultrasonic(b, PORT_4).get_sample()
+    if (sample+threshold)<baseline:
+        print "person detected at " + str(sample)+"cm"
+        im = get_image()
+	print "saving photo"
+        im.save('test'+time.strftime('%H%M%S')+'.','JPEG')
+    	time.sleep(10)
+
