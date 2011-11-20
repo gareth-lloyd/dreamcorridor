b93f9f1ac720381ea54a29deda16b4e3d7c5ac6f
Gareth Lloyd <glloyd@gmail.com>
2011-11-20 14:39:48 +0000

    add images and scripts

--- corridor/corridor.py
+++ corridor/corridor.py
@@ -8,47 +8,62 @@ from pbm import pbm_lines
 from opencv import highgui
 import opencv
 import Image
+import os
 
 colours = ["black","blue","green","yellow","red","white"]
-pbm_images = ["cloud","lightning","face","sun-cloud","rain","sun","test"]
+IMG_DIR = '../images/'
+
+def image_names():
+    for _, _, filenames in os.walk_dir(IMG_DIR):
+        return [filename for filename in filenames if not filename.startswith('.')]
+pbm_images = image_names()
 
 b = nxt.locator.find_one_brick()
 camera = highgui.cvCreateCameraCapture(1)#initial camera setup, presuming it is the 2nd (i.e. pos 1) camera
 threshold = 5 #amount of difference to trigger event
-sleeptime = 5 #sleep time to prevent multiple images
+DEAD_TIME = 3 #sleep time to prevent multiple images
+DISPLAY_TIME = 2
 baseline = Ultrasonic(b, PORT_4).get_sample() #get baseline distance measurement from sensor.
 print "baseline sampled at "+str(baseline)+"cm"
+
 def get_image():
     print "taking photo"
     im = highgui.cvQueryFrame(camera)
-    
-    #convert Ipl image to PIL image
     return opencv.adaptors.Ipl2PIL(im)
 
-def analyse_data(image,colour):
+def get_dream(image, colour):
     #convert the image and other inputs into a "dream"
-    r,g,b = image.convert("RGB").resize((1,1), Image.ANTIALIAS).getpixel((0,0))
+    r,g,bl = im.convert("RGB").resize((1,1), Image.ANTIALIAS).getpixel((0,0))
+    dream = r+g+bl+colour_sensed #clever AI algorithm
+    divisor = 780 / len(pbm_images)
+    return dream / divisor
 
-while(True):
-    sample = Ultrasonic(b, PORT_4).get_sample()
-    if (sample+threshold)<baseline:
-        
-        print "person detected at " + str(sample)+"cm"
-        colour_sensed = Color20(b,PORT_3).get_color()
-        print "LEGO Colour: "+colours[colour_sensed]
-        im = get_image()
-        r,g,bl = im.convert("RGB").resize((1,1), Image.ANTIALIAS).getpixel((0,0))
-        pic = r+g+bl+colour_sensed #clever AI algorithm
-        #write to peggy
-        divisor = 780 / len(pbm_images)
-        print "writing image "+pbm_images[pic/divisor]+" to peggy"
-        write_frame(pbm_lines('../images/'+pbm_images[pic / divisor]+'.pbm'))
-        
-        print "saving photo"
-        im.save('test'+time.strftime('%H%M%S')+'.jpg','JPEG')
-        
-        highgui.cvReleaseCapture(camera)#reset camera
-        camera = highgui.cvCreateCameraCapture(1)
-       
-    	time.sleep(sleeptime)
+def do_dream():
+    print "person detected at " + str(sample)+"cm"
+    colour_sensed = Color20(b,PORT_3).get_color()
 
+    print "LEGO Colour: "+colours[colour_sensed]
+    im = get_image()
+
+    dream = get_dream(im, colour_sensed)
+    assert 0 <= dream < len(pbm_images)
+
+    print "writing image %s to peggy" % pbm_images[dream]
+    write_frame(pbm_lines(IMG_DIR + pbm_images[dream]))
+
+    print "saving photo"
+    im.save('test'+time.strftime('%H%M%S')+'.jpg','JPEG')
+
+    highgui.cvReleaseCapture(camera)#reset camera
+    camera = highgui.cvCreateCameraCapture(1)
+
+    time.sleep(DISPLAY_TIME)
+    write_frame()
+
+    time.sleep(DEAD_TIME)
+
+if __name__ == '__main__':
+    while(True):
+        sample = Ultrasonic(b, PORT_4).get_sample()
+        if (sample + threshold) < baseline:
+            do_dream()
--- corridor/frames.py
+++ corridor/frames.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python
 from datetime import datetime
-import sys
+import os, sys
 import serial
 import time
-from random import randint
+import random
+from pbm import pbm_lines
 
 SLEEPY_TIME = 1
 START_FRAME = '^'
 END_LINE = '+'
 INPUT_DIFFERENCE = 10
+HEIGHT = 25
+BLANK_FRAME = ['\x00\x00\x00\x00' for _ in range(HEIGHT)]
 
 ser = serial.Serial('/dev/tty.usbserial-A60049W8', 28800)
 time.sleep(1)
 
-def write_frame(lines):
+def write_frame(lines=None):
+    if not lines:
+        lines = BLANK_FRAME
     ser.write(START_FRAME)
     for line in lines:
         for byte in line:
@@ -21,13 +26,17 @@ def write_frame(lines):
         ser.write(END_LINE)
 
 def write_random():
-    write_lines([''.join([chr(randint(0,255)) for x in range(4)]) for y in range(25)])
+    write_lines([''.join([chr(random.randint(0,255)) for x in range(4)]) for y in range(25)])
 
 if __name__ == '__main__':
     try:
         while(True):
-            write_random()
-            time.sleep(0.1)
+            img_dir = '../images/'
+            for dirname, dirnames, filenames in os.walk(img_dir):
+                random.shuffle(filenames)
+                if not filenames[0].startswith('.'):
+                    write_frame(pbm_lines(img_dir + filenames[0]))
+            time.sleep(2)
     except Exception, e:
         print e
     finally:
